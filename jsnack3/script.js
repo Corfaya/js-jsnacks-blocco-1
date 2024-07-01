291298ed687292d29d7511244cd1258c611c1e86
Corfaya <lauracorrado0696@gmail.com>
2024-07-01 15:34:54 +0200

    [UPD] readme update. check digits number length added in if statement. SNACK 3 DONE

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -6,13 +6,19 @@ const user_num = prompt("Inserisci un numero di 4 cifre")
 // split method to split the string coming from the prompt
 let split = user_num.split("");
 
-// initialising sum to 0
-let sum = 0;
-
-// for statement with i from 0 to 3 (split length)
-for(let i=0; i < split.length; i++) {
-    // tranform from string to number
-    let split_number = parseInt(split[i]);
-    // add the number to the sum for every iteration
-    sum += split_number
+// check split.length === 4
+if(split.length !== 4) {
+    console.log("Il numero inserito non rispetta le condizioni")
+} else {
+    // initialising sum to 0
+    let sum = 0;
+    
+    // for statement with i from 0 to 3 (split length)
+    for(let i=0; i < split.length; i++) {
+        // tranform from string to number
+        let split_number = parseInt(split[i]);
+        // add the number to the sum for every iteration
+        sum += split_number
+    }    
+    console.log(`La somma dei numeri inseriti è ${sum}`)
 }
