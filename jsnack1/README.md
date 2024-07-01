f4cc9e0eb26d0f12ae9d8c1a779d0da957eba693
Corfaya <lauracorrado0696@gmail.com>
2024-07-01 14:30:36 +0200

    [UPD] first capital letter added. SNACK 1 DONE

--- jsnack1/README.md
+++ jsnack1/README.md
@@ -1,7 +1,9 @@
 # Snack 1
 - Dichiaro un array contenente come valori i nomi degli invitati
-- Chiedo con un prompt all'utente il suo nome e salvo il valore in una variabile (name_guest), applicando il metodo toLowerCase()
+- Chiedo con un prompt all'utente il suo nome e salvo il valore in una variabile (name_guest)
+- Dichiaro una variabile per la prima lettera e una per le restanti, tale per cui la prima risulterà sempre maiuscola e il resto sempre minuscolo (uso di .slice())
+- Inizializzo una variabile che abbia come valore la somma delle due variabili di cui sopra (complete_name)
 - SE l'array include il nome inserito dall'utente
-    stampo in console `Caro/a ${name_guest}, è invitato/a alla festa del grande Gatsby. Sarebbe un onore averla come ospite.`
+    stampo in console `Caro/a ${complete_name}, è invitato/a alla festa del grande Gatsby. Sarebbe un onore averla come ospite.`
   ALTRIMENTI
-    stampo in console `Gentilissimo/a ${name_guest}, non ci risulta di averla invitata alla festa del grande Gatsby.`
+    stampo in console `Gentilissimo/a ${complete_name}, non ci risulta di averla invitata alla festa del grande Gatsby.`
--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,12 +1,17 @@
 /* In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa */
 // array
-let guests = ["fabrizio", "luigi", "alessio", "anna", "laura", "nick", "daisy"]
+let guests = ["Fabrizio", "Luigi", "Alessio", "Anna", "Laura", "Nick", "Daisy"]
 // name value
-let name_guest = prompt("Inserisca il suo nome").toLocaleLowerCase();
+let name_guest = prompt("Inserisca il suo nome");
+console.log(name_guest)
+// first capital letter
+let first_letter = name_guest.slice(0,1).toUpperCase();
+let remaining_letters = name_guest.slice(1).toLocaleLowerCase();
+let complete_name = first_letter + remaining_letters;
 
 // if statement
-if(guests.includes(name_guest)) {
-    console.log(`Caro/a ${name_guest}, è invitato/a alla festa del grande Gatsby. Sarebbe un onore averla come ospite.`)
+if(guests.includes(complete_name)) {
+    console.log(`Caro/a ${complete_name}, è invitato/a alla festa del grande Gatsby. Sarebbe un onore averla come ospite.`)
 } else {
-    console.log(`Gentilissimo/a ${name_guest}, non ci risulta di averla invitata alla festa del grande Gatsby.`)
+    console.log(`Gentilissimo/a ${complete_name}, non ci risulta di averla invitata alla festa del grande Gatsby.`)
 }
