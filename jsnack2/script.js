7e8bdf0d411c0285b238aeee9ad339d0859959ca
Corfaya <lauracorrado0696@gmail.com>
2024-07-01 14:41:35 +0200

    [UPD] for statement added

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,3 +2,8 @@
 Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell'array */
 // empty array
 let odd = [];
+
+// for statement
+for (i = 0; i < 6; i++) {
+    let num = parseInt(prompt("Inserisci un numero"));
+}
