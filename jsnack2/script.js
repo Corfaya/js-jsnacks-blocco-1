bd776619636791abb6e5cbd8c5a09dc143b4d258
Corfaya <lauracorrado0696@gmail.com>
2024-07-01 16:38:02 +0200

    fix: missing "let" declaration

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -4,7 +4,7 @@ Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo
 let odd = [];
 
 // for statement
-for (i = 0; i < 6; i++) {
+for (let i = 0; i < 6; i++) {
     let num = parseInt(prompt("Inserisci un numero"));
     // if statement
     if (num % 2 !== 0) {
