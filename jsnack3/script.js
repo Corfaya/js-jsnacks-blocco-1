cb535caf2039da47c4e8c1a4965901533e99f6e4
Corfaya <lauracorrado0696@gmail.com>
2024-07-01 15:15:37 +0200

    [UPD] prompt and prompt split added and stored into variables

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,2 +1,7 @@
 /* Chiedi un numero di 4 cifre all’utente
 e calcola la somma di tutte le cifre che compongono il numero. */
+// user value stored in a const
+const user_num = prompt("Inserisci un numero di 4 cifre")
+
+// split method to split the string coming from the prompt
+let split = user_num.split("");
